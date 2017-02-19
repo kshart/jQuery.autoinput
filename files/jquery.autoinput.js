e095cb4ce2a5bd60563ea4cb821029e7c426da75
kshart <kshart@yandex.ru>
2017-02-19 14:55:28 +0500

    Update Signed-off-by:kshart <kshart@yandex.ru>

--- files/jquery.autoinput.js
+++ files/jquery.autoinput.js
@@ -10,9 +10,10 @@
 }(function($, undefined) {
 	"use strict";
 	var _defaults = {
+		serverType:"remote",
 		serverURL:"",
-		serverMethod:"POST",
-		serverProtocol:"json",
+		serverMethod:"GET",
+		serverSendArgType:false,
 		autoUpdate:true,
 		cacheActive:false
 	};
@@ -24,6 +25,7 @@
 		that.el = el;
 		that.element = el[0];
 		that.beforeUpdateTimerID = undefined;
+		that.lastRequest;
 		that.clientLimit = {
 			pageCount:0,
 			page:1,
@@ -33,28 +35,57 @@
 		if (typeof val.clientLimitUpdate==="function") that.clientLimitUpdate = val.clientLimitUpdate;
 		if (val.serverType==="local") {
 			that.localRequest = val.localRequest;
-		}else{
+		}else if (val.serverType==="remote"){
 			that.cacheActive = val.cacheActive===true?true:false;
 			that.server = {
 				url:val.serverURL,
 				method:val.serverMethod,
-				protocol:val.serverProtocol,
+				sendArgType:val.serverSendArgType,
 				xhr:new XMLHttpRequest()
 			};
+			that.server.xhr.timeout = 10000;
+			
+			
+			if (typeof val.serverOnEndLoad==="function") {
+				that.server.xhr.onload = function(e) {
+					val.serverOnEndLoad();
+					that.serverLoadedItems();
+				};
+			}else{
+				that.server.xhr.onload = function(e) {
+					that.serverLoadedItems();
+				};
+			}
+			
+			if (typeof val.serverOnStartLoad==="function") {
+				that.server.xhr.onloadstart = function(e) {
+					val.serverOnStartLoad();
+				};
+			}
+			if (typeof val.serverOnProgressLoad==="function") {
+				that.server.xhr.onprogress = function(e) {
+					val.serverOnProgressLoad(e);
+				};
+			}
+			if (typeof val.serverOnError==="function") {
+				that.server.xhr.ontimeout = that.server.xhr.onabort = that.server.xhr.onerror = function(e) {
+					val.serverOnError();
+				};
+			}
 		}
 		if (val.autoUpdate===true) {
 			if (typeof val.clientAutoUpdateTimeoutMS==="number") that.clientAutoUpdateTimeoutMS = val.clientAutoUpdateTimeoutMS;
 			$("[data-ai-name]", this.el).each(function(i, el) {
 				var tag = el.tag,
 					type = el.getAttribute("data-ai-type"),
-					bUpdate = function(){that.beforeUpdate()};
+					bUpdate = function(){that.beforeUpdate();};
 				switch(type) {
 					case "text":
 						if ("onpropertychange" in el) {
 							el.onkeyup = el.oninput = bUpdate;
 							el.onpropertychange = function(event) {
 								if (event.propertyName==="value") bUpdate();
-							}
+							};
 							el.oncut = function() {
 								setTimeout(bUpdate, 0);
 							};
@@ -114,7 +145,7 @@
 		beforeUpdate:function() {
 			if (this.beforeUpdateTimerID===undefined) {
 				var that = this;
-				setTimeout(function(){that.update();}, that.clientAutoUpdateTimeoutMS);
+				this.beforeUpdateTimerID = setTimeout(function(){that.update();}, that.clientAutoUpdateTimeoutMS);
 			}
 		},
 		update:function() {
@@ -164,12 +195,77 @@
 				}
 			}else{
 				//onClosed event
-				this.xhr.abort();
+				this.server.xhr.abort();
+				if (this.server.method==="POST") {
+					this.server.xhr.open("POST", this.server.url);
+					if (this.server.sendArgType) {
+						this.server.xhr.send(request);
+					}else{
+						var requestWithoutArgType = {};
+						for(var i in request) {
+							requestWithoutArgType[i] = {value:request.value};
+						}
+						this.server.xhr.send(requestWithoutArgType);
+					}
+				}else{//GET
+					var getStr = "?";
+					if (this.server.sendArgType) {
+						for(var i in request) {
+							getStr += encodeURIComponent(i)+"="+encodeURIComponent(request[i].value)+"&"+i+"type="+encodeURIComponent(request[i].type)+"&";
+						}
+					}else{
+						for(var i in request) {
+							switch (request[i].type) {
+								case "checkbox":
+									getStr += encodeURIComponent(i)+"="+(request[i].value===true?"1":"0")+"&";
+									break;
+								case "text":
+								case "radio":
+								default:
+									getStr += encodeURIComponent(i)+"="+encodeURIComponent(request[i].value)+"&";
+							}
+						}
+					}
+					if (this.clientLimit.itemsOnPage!==0) {
+						getStr += "page="+Math.ceil(this.clientLimit.page)+"&itemsonpage="+Math.ceil(this.clientLimit.itemsOnPage);
+					}else{
+						getStr = getStr.substr(0, getStr.length-1);
+					}
+					this.server.xhr.open("GET", this.server.url+getStr);
+					this.server.xhr.send(null);
+				}
 				
 			}
+		},
+		serverLoadedItems:function() {
+			var response;
+			switch (this.server.xhr.responseType) {
+				case "json":
+					response = this.server.xhr.response;
+					break;
+				case "text":
+				default:
+					response = JSON.parse(this.server.xhr.response);
+					break;
+			}
+			if (typeof response.limit==="object") {
+				if (typeof response.limit.pageCount==="number"||
+					typeof response.limit.page==="number"||
+					typeof response.limit.itemsOnPage==="number") {
+					this.clientLimit.pageCount = Math.ceil(response.limit.pageCount);
+					this.clientLimit.page = Math.ceil(response.limit.page);
+					this.clientLimit.itemsOnPage = Math.ceil(response.limit.itemsOnPage);
+				}
+			}
+			if (typeof response.items==="undefined") response.items = [];
+			var el = $("[data-ai-output]", this.el).first(), elLimit;
+			if (el[0]!==undefined) {
+				this.clientViewData(el[0], response.items);
+				elLimit = $("[data-ai-limit]", this.el).first();
+				if (elLimit[0]!==undefined) this.clientLimitUpdate(elLimit[0], this.clientLimit);
+			}
+			console.log(response);
 		}
-		
-		
 	};
 	
 	$.fn.AutoInput = function (obj, arg1, arg2) {
